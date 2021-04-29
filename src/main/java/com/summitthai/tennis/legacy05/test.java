f4c60049e5118eb0041511bb3661dc26d04d2601
terex junjaroen <terex2004@gmail.com>
2021-04-29 11:58:46 +0700

    initial

--- src/main/java/com/summitthai/tennis/legacy05/test.java
+++ src/main/java/com/summitthai/tennis/legacy05/test.java
@@ -5,6 +5,10 @@
  */
 package com.summitthai.tennis.legacy05;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 /**
  *
  * @author terex
@@ -16,6 +20,60 @@ public class test {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+                
+        List<String> lscore = new ArrayList<String>();
+        lscore.add("0-0");
+        lscore.add("0-15");
+        lscore.add("15-15");
+        lscore.add("30-15");
+        lscore.add("30-30");
+        lscore.add("40-30");
+        lscore.add("40-40");
+        lscore.add("40-40");
+        lscore.add("40-40");
+        lscore.add("40-40");
+        
+        int countdeuce = 0;
+        for(String score : lscore){
+            String[] scores = score.split("-");
+            
+            if(scores[0].equals(scores[1])){
+                countdeuce++;
+            }else{
+                countdeuce = 0;
+            }
+            
+            String msg = callScore(scores[0],scores[1]);
+            if(countdeuce >= 1){
+                if(countdeuce == 4){
+                    System.out.println("P1 advantage");
+                }else{
+                    if(scores[0].equals("40") || scores[1].equals("40")){
+                        System.out.println(msg+" deuce "+countdeuce);
+                    }else{
+                        System.out.println(msg);
+                    }
+                }
+            }else{
+                System.out.println(msg);
+            }
+            
+            
+        }
+        
     }
+    
+    public static String callScore(String p1,String p2){
+        
+        HashMap<String,String> hm = new HashMap<String,String>();
+        hm.put("0", "love");
+        hm.put("15", "fifteen");
+        hm.put("30", "thirty");
+        hm.put("40", "forty");
+                        
+        return hm.get(p1)+":"+hm.get(p2);
+        
+    }
+    
     
 }
