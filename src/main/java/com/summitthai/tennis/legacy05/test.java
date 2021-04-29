f6620b3d4e4afa5298a6f33836f4e333a3ca2c0b
terex junjaroen <terex2004@gmail.com>
2021-04-29 10:34:55 +0700

    initial

---
+++ src/main/java/com/summitthai/tennis/legacy05/test.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.summitthai.tennis.legacy05;
+
+/**
+ *
+ * @author terex
+ */
+public class test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
